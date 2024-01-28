3e6ef5048d6d85574f3042e69d2a16d53c24e4c6
SeraphelCelaena <minecraft6401@hotmail.com>
2024-01-28 14:45:02 -0700

    finished ass1

--- Assignment-1/ui_layout/lib/main.dart
+++ Assignment-1/ui_layout/lib/main.dart
@@ -31,76 +31,84 @@ class MyApp extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             //TODO: Put your code here to complete this app.
-            Column(
-              children: <Widget>[
-                Container(
+            Column(children: <Widget>[
+              Container(
                   width: 106,
                   height: 106,
                   decoration: BoxDecoration(
-                    color: Colors.amber,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 3
-                    )
-                  ),
+                      color: Colors.amber,
+                      border: Border.all(color: Colors.black, width: 3)),
                   child: const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      child: Text("Container 1")
-                    )
-                  )
-                ),
-                Transform.rotate(
+                      padding: EdgeInsets.all(10.0),
+                      child: Align(child: Text("Container 1")))),
+              Transform.rotate(
                   angle: pi / 4,
                   child: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.white,
-                    child: const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Align(
-                        child: Text("Container 2")
-                      )
-                    )
-                  )
-                )
-              ]
-            ),
-            Column(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Container(
                       width: 100,
                       height: 100,
-                      color: Colors.yellow,
-                      child: const Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Text("Container 3"),
-                      )
-                    )
-                  )
-                ),
-                Expanded(
+                      color: Colors.white,
+                      child: const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Align(child: Text("Container 2")))))
+            ]),
+            Column(children: <Widget>[
+              Expanded(
                   flex: 1,
                   child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.yellow,
+                          child: const Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text("Container 3"),
+                          )))),
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.blue,
+                          child: const Align(
+                            alignment: Alignment.centerRight,
+                            child: Text("Container 4"),
+                          ))))
+            ]),
+            Column(children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.blue,
-                      child: const Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Text("Container 3"),
-                      )
-                    )
-                  )
-                )
-              ]
-            ),
-            Column()
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.white, width: 3)),
+                        child: const Align(
+                          alignment: Alignment.center,
+                          child: Text("Container 5",
+                              style: TextStyle(color: Colors.white)),
+                        ))),
+              ),
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.red,
+                          child: const Align(
+                            alignment: Alignment.topLeft,
+                            child:
+                                Text("Con 6", style: TextStyle(fontSize: 30)),
+                          ))))
+            ])
           ],
         ),
       ),
