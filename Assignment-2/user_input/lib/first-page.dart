2d7c3e6f7d4a6c66097519bf6a8073b10cdbc29c
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2024-02-26 19:53:18 -0700

    finished Assignment 2

--- Assignment-2/user_input/lib/first-page.dart
+++ Assignment-2/user_input/lib/first-page.dart
@@ -37,16 +37,13 @@ class MyFirstPageState extends State<MyFirstPage> {
               //TODO: Replace this Text Widget
               // and build the label and switch here
               // as children of the row.
-              ListTile(
-                title: Text('Enable Buttons'),
-                leading: Switch(
-                  value: enabled,
-                  onChanged: (bool value) {
-                    setState(() {
-                      enabled = !enabled;
-                    });
-                  },
-                ),
+              const Text('Enable Buttons'),
+              Switch(
+                value: enabled,
+                onChanged: (bool value) {
+                  enabled = value;
+                  setState(() {});
+                },
               )
             ],
           ),
@@ -58,6 +55,26 @@ class MyFirstPageState extends State<MyFirstPage> {
               // For each button use a
               // "Visibility Widget" and its child
               // will be an "ElevatedButton"
+              Visibility(
+                  visible: enabled,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      timesClicked++;
+                      setState(() {});
+                    },
+                    child: timesClicked == 0
+                        ? const Text('Click Me')
+                        : Text('Clicked $timesClicked'),
+                  )),
+              Visibility(
+                  visible: enabled,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      timesClicked = 0;
+                      setState(() {});
+                    },
+                    child: const Text('Reset'),
+                  )),
             ],
           ),
           const SizedBox(
@@ -77,6 +94,49 @@ class MyFirstPageState extends State<MyFirstPage> {
                   // a submit button that will show a
                   // snackbar with the "firstName"
                   // if validation is satisfied.
+                  TextFormField(
+                    controller: textEditingController,
+                    maxLength: 20,
+                    decoration: const InputDecoration(
+                        icon: Icon(Icons.hourglass_top),
+                        suffixIcon: Icon(Icons.check_circle),
+                        hintText: 'First Name',
+                        helperText: 'min 1, max 20'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your first name';
+                      } else if (value.length > 20) {
+                        return 'First name must be less than 20 characters';
+                      } else {
+                        return null;
+                      }
+                    }),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Row(
+                              children: [
+                                Icon(Icons.favorite, color: Colors.white),
+                                Text('Hey There, Your name is ${textEditingController.text}')
+                                ]
+                              ),
+                            duration: const Duration(seconds: 5),
+                            action: SnackBarAction(
+                              label: 'Click Me',
+                              textColor: Colors.white,
+                              onPressed: () {
+                                print('Click Me Pressed');
+                              },
+                            ),
+                          ),
+                        );
+                        setState(() {});
+                      }
+                    },
+                    child: const Text('Submit'),
+                  )
                 ],
               ),
             ),
